203cf7a5b13022f15b95916ba1da314151335e29
RattinSadman <r.sadman@dviz.com>
2025-11-17 02:59:27 +0600

    CHANGE MENIFESTO SECTION STYLES

--- src/components/Manifesto.jsx
+++ src/components/Manifesto.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Manifesto = () => {
   return (
-    <div className="bg-[#171618] pb-16 md:pb-24 lg:pb-32">
-      <div className="max-w-6xl mx-auto px-12 md:px-16 lg:px-20">
+    <div className="bg-red-800 w-screen h-screen flex items-center justify-center">
+      <div className="w-[90%] h-[90%] bg-yellow-500 flex items-center justify-center px-12 md:px-16 lg:px-20">
         <p className="text-3xl md:text-4xl lg:text-5xl font-semibold text-white text-center leading-relaxed tracking-wide">
           "Nibiro makes imagination tangible. We craft products that honor our
           heritage while building our future—where your story becomes art, and
