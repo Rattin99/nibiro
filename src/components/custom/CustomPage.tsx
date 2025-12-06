3103e979e5785b274e45aefa3a62cc843b00cd10
RattinSadman <r.sadman@dviz.com>
2025-12-06 20:50:06 +0600

    CHANGE STYLE OF CUSTOM PAGE

--- src/components/custom/CustomPage.tsx
+++ src/components/custom/CustomPage.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from 'react';
-import CustomHero from './CustomHero';
+import React from "react";
+import CustomHero from "./CustomHero";
 import ProcessSteps from './ProcessSteps';
 import OrderForm from './OrderForm';
+import TestimonialScroll from './TestimonialScroll';
 
 const CustomPage = () => {
   return (
@@ -16,24 +17,8 @@ const CustomPage = () => {
           <div className="w-full xl:w-1/2">
              <ProcessSteps />
              
-             {/* Additional Info / Trust Signals */}
-             <div className="mt-16 p-8 bg-white border-2 border-black rounded-xl shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] relative">
-                <h3 className="text-2xl font-black font-montserrat mb-4 text-black">Why Go Custom?</h3>
-                <ul className="space-y-4 text-lg text-neutral-800">
-                  <li className="flex items-center gap-3">
-                    <span className="w-8 h-8 bg-red-500 rounded-lg flex items-center justify-center text-white text-xl font-black border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]">+</span>
-                    Unique gifts that can't be bought in stores.
-                  </li>
-                  <li className="flex items-center gap-3">
-                    <span className="w-8 h-8 bg-blue-500 rounded-lg flex items-center justify-center text-white text-xl font-black border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]">+</span>
-                    Immortalize special moments in 3D.
-                  </li>
-                  <li className="flex items-center gap-3">
-                    <span className="w-8 h-8 bg-yellow-400 rounded-lg flex items-center justify-center text-black text-xl font-black border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]">+</span>
-                    Collaborate directly with digital sculptors.
-                  </li>
-                </ul>
-             </div>
+             {/* Testimonial Scroller */}
+             <TestimonialScroll />
           </div>
 
           {/* Right Side: The Order Form */}
